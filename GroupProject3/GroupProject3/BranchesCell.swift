f54c42677f1c5f7ec515a7eb5d02b921d49b1582
salome sulkhanishvili <sulkhanishvili767@gmail.com>
2020-06-19 17:57:50 +0400

    salome

---
+++ GroupProject3/GroupProject3/BranchesCell.swift
@@ -0,0 +1,26 @@
+//
+//  BranchesCell.swift
+//  GroupProject3
+//
+//  Created by ekaterine iremashvili on 6/19/20.
+//  Copyright © 2020 TBC. All rights reserved.
+//
+
+import UIKit
+
+class BranchesCell: UITableViewCell {
+
+    @IBOutlet weak var atmNameLabel: UILabel!
+    @IBOutlet weak var atmAdressLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
