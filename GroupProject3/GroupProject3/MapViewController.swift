91f7e33dffc2034c7c4a47852ceb6e97405ee1ff
georgedigmelashvili <georg.digmelashvili@gmail.com>
2020-06-19 17:50:01 +0400

    mapView

--- GroupProject3/GroupProject3/MapViewController.swift
+++ GroupProject3/GroupProject3/MapViewController.swift
@@ -11,58 +11,67 @@ import CoreLocation
 import MapKit
 
 class MapViewController: UIViewController {
-
-  let locationManager = CLLocationManager()
-  @IBOutlet var mapView: MKMapView!
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    checkAuthorizationStatus()
-    //addAnnotation(api: list)
-  }
-  private func addAnnotation(api: List? = nil) {
-    guard let locations = api else {return}
-    for loc in locations.objects{
-      let lat = Double(loc.latitude)!
-      let lng = Double(loc.longtitude)!
-      let annotation = MKPointAnnotation()
-      annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(exactly: lat)!, longitude: CLLocationDegrees(exactly: lng)!)
-      annotation.title = loc.nameGe
-      self.mapView.addAnnotation(annotation)
+    
+    let locationManager = CLLocationManager()
+    @IBOutlet var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        checkLocationServiceEnabled()
+        //addAnnotation(api: list)
     }
-  }
-  private func checkLocationServiceEnabled() {
-    if CLLocationManager.locationServicesEnabled() {
-      setupLocationManager()
-      checkAuthorizationStatus()
-    } else {
+    private func addAnnotation(api: List? = nil) {
+        guard let locations = api else {return}
+        for loc in locations.objects{
+            let lat = Double(loc.latitude)!
+            let lng = Double(loc.longtitude)!
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(exactly: lat)!, longitude: CLLocationDegrees(exactly: lng)!)
+            annotation.title = loc.nameGe
+            self.mapView.addAnnotation(annotation)
+        }
     }
-  }
-  private func setupLocationManager() {
-    locationManager.delegate = self
-    locationManager.desiredAccuracy = kCLLocationAccuracyBest
-  }
-  private func checkAuthorizationStatus() {
-    switch CLLocationManager.authorizationStatus() {
-    case .authorizedWhenInUse:
-      locationManager.startUpdatingLocation()
-      mapView.showsUserLocation = true
-    case .authorizedAlways:
-      break
-    case .denied:
-      break
-    case .notDetermined:
-      locationManager.requestWhenInUseAuthorization()
-    case .restricted:
-      break
-    @unknown default:
-      fatalError()
+    private func checkLocationServiceEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkAuthorizationStatus()
+        } else {
+        }
     }
-  }
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    private func checkAuthorizationStatus() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+            mapView.showsUserLocation = true
+        case .authorizedAlways:
+            break
+        case .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    private func centerMapView() {
+        let mapMeterds: Double = 5_000
+        guard let location = locationManager.location else {return}
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: mapMeterds, longitudinalMeters: mapMeterds)
+        mapView.setRegion(region, animated: true)
+    }
 }
 
-extension MapViewController: CLLocationManagerDelegate{
-  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    manager.stopUpdatingLocation()
-  }
+extension MapViewController: CLLocationManagerDelegate, MKMapViewDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        centerMapView()
+        manager.stopUpdatingLocation()
+    }
 }
